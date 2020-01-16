edf085fbd1691b388b8fa5e302df4eb6d3c6a0dd
michelletiffny <icel125@uw.edu>
2020-01-16 14:41:54 -0800

    Add week 2 exercises

--- chapter-07-exercises/exercise-1/exercise.R
+++ chapter-07-exercises/exercise-1/exercise.R
@@ -2,7 +2,7 @@
 
 # Create a vector `names` that contains your name and the names of 2 people 
 # next to you. Print the vector.
-names <- c("Chesie, Alex")
+names <- c("Chesie", "Alex")
 print(names)
 
 # Use the colon operator : to create a vector `n` of numbers from 10:49
